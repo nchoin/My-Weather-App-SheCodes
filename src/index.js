8203b5adc579789fec31e90117c6168e93661a45
nchoin <139597297+nchoin@users.noreply.github.com>
2024-01-17 13:46:54 -0500

    added beginning JS function to get city entered. added link to axios to the function can use the information from the API.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+// this function takes the searchCity.value from the sendCitySubmitted function because we used it as an argument, and will add the city to the weatherApiUrl. It will use the response.data get all the information we need to display.  
+
+function searchForCity(city){
+    let weatherApiKey = "6bccfefa354f0f4do4245dc0a56fata0";
+    let weatherApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${weatherApiKey}&units=imperial`;
+    
+    
+}
+
+// created the basic function to target the search city and be able to change the html to display the city name. 
+function sendCitySubmitted(event){
+  event.preventDefault();
+  let searchCity = document.querySelector("#city-entered");
+
+//   let changeCityElement = document.querySelector("#weather-city");
+
+//   changeCityElement.textContent = searchCity.value;
+  console.log(searchCity.value);
+  searchForCity(searchCity.value)
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", sendCitySubmitted);
+
+
+
+
+
+// formatting with dates
 let date = document.querySelector("#date");
 let today = new Date();
 let localFormat = today.toLocaleString();
